015ecd975f0cc8c4583e4dedefe50c9f57661fae
Ammar Shahab <ammar.qa@generalpharma.com>
2025-05-27 15:57:18 +0600

    uodated some comments

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
-
 // 1.0 iniitial server setup by $ mkdir mil-11-m-58-career-code-job-server => cd mil-11-m-58-career-code-job-server =>  npm init => then install in one command "npm i express mongodb nodemon cors dotenv"
 
-const cors = require('cors');
-const express = require('express');
+const cors = require("cors");
+const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 // 3.3 due import move to the up
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 // 2.0 mannually some data is input in data base. For this first go to mongodb server => cluster => browse collection => create database => fill the Database Name => Collection Name => then create. After created, in jobs u will find insert document. click on it and delete any data like ObjectID is present then paste ur data.
 
-
 // 1.2 middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // 4.0 for add new database user and password go to database access => Add New Database User => in Password Authentication enter user name: 'career_db_admin and in password use auto generated password which is "O4t3tOchGoC21XpN". Then Built-in Role will be admin then add user.
 
-// 4.1 as we have to hide the user name and password so create .env file then type DB_USER=career_db_admin and DB_PASS=O4t3tOchGoC21XpN 
+// 4.1 as we have to hide the user name and password so create .env file then type DB_USER=career_db_admin and DB_PASS=O4t3tOchGoC21XpN
 
 // 3.0 now connect the server by going to cluster => connect => driver => copy the code and follow 3.1
 
@@ -33,18 +31,38 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
-
 
 // 3.1
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // 5.1
+    // Get the database and collection on which to run the operation
+    const jobsCollections = client.db("carrerCode").collection("jobs");
+
+    // 5.2 send the hotjobs data to ui after that u will find the data manually by typing in browser url "http://localhost:3000/jobs"
+    app.get("/jobs", async (req, res) => {
+      const cursor = await jobsCollections.find().toArray();
+      res.send(cursor);
+    });
+
+    // 6.3 make the api for get the data by id
+    app.get("/jobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobsCollections.findOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close(); // 3.2 it must be commented
@@ -52,17 +70,12 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
 // 1.4 now in server terminal type nodemon index.js
-app.get('/', (req, res)=> {
-    res.send('Job server is running')
-  })
-  
+app.get("/", (req, res) => {
+  res.send("Job server is running");
+});
 
-  
-app.listen(port, ()=> {
-    console.log(`Job server is running on port ${port}`)
-    //1.5 now in server terminal type nodemon index.js also type in browser url http://loclhost:3000
-  })
-
+app.listen(port, () => {
+  console.log(`Job server is running on port ${port}`);
+  //1.5 now in server terminal type nodemon index.js also type in browser url http://loclhost:3000
+});
